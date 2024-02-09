8b975cdb28b85000ba5e95b60488bbda096347ac
elan_rubin <elandanielrubin@gmail.com>
2024-02-08 19:35:15 -0500

    edited page

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const foods = [
-    { image: "image1.jpg", name: "Dish 1", description: "Description for Dish 1" },
-    { image: "image2.jpg", name: "Dish 2", description: "Description for Dish 2" },
-    { image: "image3.jpg", name: "Dish 3", description: "Description for Dish 3" },
+    { image: "image1.png", name: "Burger", description: "Description for Dish 1" },
+    { image: "image2.png", name: "Pasta", description: "Description for Dish 2" },
+    { image: "image3.png", name: "Salad", description: "Description for Dish 3" },
 ];
 
 let currentIndex = 0;
-let likedFoods = [];
+let likedFoods = [], dislikedFoods = [];
 
 function swipe(direction) {
     const foodImage = document.getElementById('foodImage');
@@ -13,22 +13,32 @@ function swipe(direction) {
     const description = document.getElementById('description');
 
     if (direction === 'left') {
+        dislikedFoods.push(foods[currentIndex]);
         currentIndex = (currentIndex - 1 + foods.length) % foods.length;
+        updateDislikedFoods();
     } else if (direction === 'right') {
         likedFoods.push(foods[currentIndex]);
         currentIndex = (currentIndex + 1) % foods.length;
+        updateLikedFoods();
     }
 
-    updateLikedFoods();
-    updateFoodDisplay();
+    updateFoodDisplay(direction);
 }
 
-function updateFoodDisplay() {
+function updateFoodDisplay(direction) {
     const foodImage = document.getElementById('foodImage');
     const dishName = document.getElementById('dishName');
     const description = document.getElementById('description');
 
-    foodImage.style.transform = 'translateX(100%)';
+    translation = ''
+    if (direction === 'left') {
+        translation = 'translateX(-100%)'
+    }
+    else {
+        translation = 'translateX(100%)'
+    }
+
+    foodImage.style.transform = translation;
     setTimeout(() => {
         foodImage.src = foods[currentIndex].image;
         dishName.innerText = foods[currentIndex].name;
@@ -38,14 +48,52 @@ function updateFoodDisplay() {
 }
 
 function updateLikedFoods() {
-    const likedFoodsContainer = document.getElementById('likedFoods');
-    likedFoodsContainer.innerHTML = '';
-
-    likedFoods.forEach(food => {
-        const likedFoodElement = document.createElement('img');
-        likedFoodElement.src = food.image;
-        likedFoodElement.alt = 'Liked Food';
-        likedFoodElement.className = 'likedFood';
-        likedFoodsContainer.appendChild(likedFoodElement);
-    });
+    updateFoodPref("likedFood")
 }
+
+function updateDislikedFoods() {
+    updateFoodPref("dislikedFood")
+}
+
+function updateFoodPref(name) {
+    const likedFoodsContainer = document.getElementById("likedFoods");
+
+    const dislikedFoodsContainer = document.getElementById("dislikedFoods");
+
+    if (name === "likedFood") {
+        likedFoodsContainer.innerHTML = '';
+        likedFoods.forEach(food => {
+            const foodElement = document.createElement('img');
+            foodElement.src = food.image;
+            // likedFoodElement.alt = 'Liked Food';
+            foodElement.className = name;
+            likedFoodsContainer.appendChild(foodElement);
+        });
+    }
+    //dislikedFood
+
+    //THIS CAN BE SIMPLIFIED LATER
+    else {
+        dislikedFoodsContainer.innerHTML = '';
+        dislikedFoods.forEach(food => {
+            const foodElement = document.createElement('img');
+            foodElement.src = food.image;
+            // likedFoodElement.alt = 'Liked Food';
+            foodElement.className = name;
+            dislikedFoodsContainer.appendChild(foodElement);
+        });
+    }
+}
+
+// function updateLikedFoods() {
+//     const likedFoodsContainer = document.getElementById('likedFoods');
+//     likedFoodsContainer.innerHTML = '';
+
+//     likedFoods.forEach(food => {
+//         const likedFoodElement = document.createElement('img');
+//         likedFoodElement.src = food.image;
+//         likedFoodElement.alt = 'Liked Food';
+//         likedFoodElement.className = 'likedFood';
+//         likedFoodsContainer.appendChild(likedFoodElement);
+//     });
+// }
