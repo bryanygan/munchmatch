7780e784fd841745b34ce90a78b916e8b16a2e5b
elan_rubin <elandanielrubin@gmail.com>
2024-03-06 02:10:53 -0500

    food list and combing algorithm added

---
+++ image_comber.py
@@ -0,0 +1,26 @@
+foodsText = open("foods_list.txt", "r")
+foodsLines = foodsText.readlines()
+outputLines = []
+allFoods = []
+
+for line in foodsLines:
+    for food in line.split(","):
+        allFoods.append(food)
+#remove dups
+allFoods = list(set(allFoods))
+
+counter = 0
+for food in allFoods:
+    outputLines.append(food.strip().strip('\n'))
+    counter+=1
+    if(counter%10==0):
+        outputLines.append("\n")
+    else:
+        outputLines.append(",")
+
+combedText = open("foods_list_combed.txt", "w")
+outputText = ""
+for line in outputLines:
+    outputText += line
+combedText.writelines(outputText)
+combedText.close()
